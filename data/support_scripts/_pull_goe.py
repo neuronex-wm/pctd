bcdb77901e9e3a1f4708d4822f5158ba5040c9c9
smestern <smestern@gmail.com>
2026-03-05 11:13:59 -0500

    add goe cells and hope github dosen't panic

---
+++ data/support_scripts/_pull_goe.py
@@ -0,0 +1,28 @@
+import pandas
+import os
+import shutil
+import glob
+
+id_lookup_csv = r"C:\Users\SMest\source\pctd\data\box2_ephys.csv"
+traces_dir = r"C:\Users\SMest\Downloads\DATA_NXWM_GOE_2025\traces"
+new_traces_dir = r"C:\Users\SMest\source\pctd\data\traces"
+def main():
+    #read the csv
+    df = pandas.read_csv(id_lookup_csv)
+    #create a dictionary from the csv
+    id_dict = dict(zip(df['internalID'], df['cellID']))
+    #glob them csvs
+    traces = glob.glob(os.path.join(traces_dir, "*.csv"))
+    #for each file in the traces directory
+    for file in traces:
+        if file.endswith(".csv"):
+            internal_id = os.path.basename(file).split('.')[0]
+            if internal_id in id_dict:
+                cell_id = internal_id
+                new_file_name = cell_id + ".csv"
+                shutil.copy(os.path.join(traces_dir, file), os.path.join(new_traces_dir, new_file_name))
+                print(f"Copied {file} to {new_file_name}")
+    return
+
+if __name__ == "__main__":   
+    main()
