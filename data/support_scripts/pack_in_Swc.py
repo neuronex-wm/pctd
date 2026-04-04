cce60c91505b818f1b39e52902d97dab595e95a2
smestern <smestern@gmail.com>
2026-04-04 17:56:24 -0400

    Add SWC file packing script and enhance DANDI preparation script with electrode linking

---
+++ data/support_scripts/pack_in_Swc.py
@@ -0,0 +1,75 @@
+# Here we will attemp to load pack the SWC files into the nwbs, but this is not a priority
+
+import glob
+import pandas
+import pynwb
+import os
+from pynwb import NWBHDF5IO
+from hdmf.common import DynamicTable, VectorData
+
+NWB_DIR = r"C:\Users\SMest\source\pctd\data\updated_nwbs"
+NWB_OUT_DIR = r"C:\Users\SMest\source\pctd\data\nwbs_with_morph"
+
+CSV_FILE = r"C:\Users\SMest\source\pctd\data\box2_ephys.csv"
+
+SWC_DIR = r"C:\Users\SMest\Downloads\NXWM NC Paper - Morpho\NXWM NC Paper - Morpho"
+
+def main():
+    df = pandas.read_csv(CSV_FILE)
+    nwbs = glob.glob(os.path.join(NWB_DIR, "**/*.nwb"), recursive=True)
+    swcs = glob.glob(os.path.join(SWC_DIR, "*.swc"))
+    for nwb in nwbs: #reverse the list of nwbs so we start with the most recent ones first, since those are more likely to have swc files. 
+        swc_file = None #reset so we don't accidentally use the same swc file for multiple nwbs if there is an error in the code and we don't find a swc file for a particular nwb. This way we won't accidentally add the same swc file to multiple nwbs.
+        #nwbs are labeled by cellID, swcs are by internalID, so we need to get the internalID from the cellID using the provided CSV
+        cell_id = os.path.basename(nwb).split('.')[0]
+        internal_id = df[df['cellID'] == int(cell_id)]['internalID'].values[0]
+        #find the corresponding swc file    
+        swc_file = [swc for swc in swcs if os.path.basename(swc).split('.')[0] == internal_id] #not all cells have swc files, so this will throw an error if there is no swc file for the cell. We can catch this error and just skip the cell if there is no swc file.
+        if swc_file or len(swc_file) > 0: #if there is a swc file for the cell
+            swc_file = swc_file[0]
+            #load the nwb file
+            nwb_file = pynwb.NWBHDF5IO(nwb, 'a')
+            nwb_data = nwb_file.read()
+            # Just keeps the PI's happy
+            subj = internal_id[:3]
+            if not os.path.exists(os.path.join(NWB_OUT_DIR, subj)):
+                os.makedirs(os.path.join(NWB_OUT_DIR, subj))
+                out_path = os.path.join(NWB_OUT_DIR, subj, os.path.basename(nwb))
+            else:
+                out_path = os.path.join(NWB_OUT_DIR, subj, os.path.basename(nwb))
+            #load the swc file, its actually just a space delimited text file, so we can use pandas to read it in
+            #sometimes it has comments at the top, so we need to skip those. We can do this by specifying the comment character in pandas read_csv, which is '#'
+            swc_data = pandas.read_csv(swc_file, sep=' ', header=None, names=['Index', 'Type', 'X', 'Y', 'Z', 'Radius', 'Parent'], comment='#')
+            #make a new DynamicTable to hold the swc data
+            index_col = VectorData(name='Index', description='Index of the SWC point', data=swc_data['Index'].values)
+            type_col = VectorData(name='Type', description='Type of the SWC point', data=swc_data['Type'].values)
+            x_col = VectorData(name='X', description='X coordinate of the SWC point, in microns', data=swc_data['X'].values)
+            y_col = VectorData(name='Y', description='Y coordinate of the SWC point, in microns', data=swc_data['Y'].values)
+            z_col = VectorData(name='Z', description='Z coordinate of the SWC point, in microns', data=swc_data['Z'].values)
+            radius_col = VectorData(name='Radius', description='Radius of the SWC point, in microns', data=swc_data['Radius'].values)
+            parent_col = VectorData(name='Parent', description='Index of the parent SWC point, -1 if there is no parent', data=swc_data['Parent'].values)
+            swc_table = DynamicTable(name='SWC', description='SWC data from '+ os.path.basename(swc_file), columns=[index_col, type_col, x_col, y_col, z_col, radius_col, parent_col])
+
+            nwb_data.create_processing_module(name='morphology', description='Module to hold morphology data')
+            #add to the processing subdir
+            nwb_data.processing['morphology'].add(swc_table)
+            #save the new nwb file for recording keeping, we will save it in a new directory so we don't accidentally overwrite the original nwbs
+
+            with NWBHDF5IO(out_path, mode="w") as export_io:
+                export_io.export(src_io=nwb_file, nwbfile=nwb_data)
+
+
+            nwb_file.close()
+            #test read
+            with pynwb.NWBHDF5IO(out_path, 'r') as io:
+                nwb_data = io.read()
+                print(nwb_data.processing['morphology']['SWC'])
+                print(f"X col:{nwb_data.processing['morphology']['SWC']['X'].data[:10]}")
+
+            
+
+
+
+
+if __name__ == "__main__":
+    main()
--- data/support_scripts/prep_for_dandi.py
+++ data/support_scripts/prep_for_dandi.py
@@ -5,11 +5,35 @@ import glob
 import pandas
 import os
 import shutil
+import h5py
+import pynwb
+from pynwb.icephys import IntracellularElectrode, Device
+print(pynwb.__version__)
 
-data_folders = [r"C:\Users\SMest\Downloads\DATA_NXWM_GOE_2025\DATA_NXWM_GOE_2025", r"G:\Data4Website" ]
+data_folders = [r"G:\Data4Website" ]
 
 id_lookup_csv = r"C:\Users\SMest\source\pctd\data\box2_ephys.csv"
-new_nwb_dir = r"C:\Users\SMest\source\pctd\data\nwbs"
+new_nwb_dir = r"C:\Users\SMest\source\pctd\data\updated_nwbs"
+
+
+def pynwb_check(nwb_file):
+    import pynwb
+    try:
+        with pynwb.NWBHDF5IO(nwb_file, 'r') as io:
+            nwb_data = io.read()
+            print(f"Successfully read {nwb_file} with pynwb.")
+            return True
+    except Exception as e:
+        print(f"Error reading {nwb_file} with pynwb: {e}")
+        return False
+
+def find_electrode_group(nwb):
+    if '/general/intracellular_ephys' in nwb:
+        for key in nwb['/general/intracellular_ephys']:
+            if 'unknown_electrode' in key.lower() or 'intracellular_electrode' in key.lower() or 'electrode' in key.lower():
+                return nwb['/general/intracellular_ephys'][key], key
+    return None, None
+
 
 def main():
     #read the csv
@@ -33,6 +57,76 @@ def main():
                 cell_id = id_dict[internal_id]
                 new_file_name = str(cell_id) + ".nwb"
                 shutil.copy(os.path.join(folder, nwb), os.path.join(new_nwb_dir, subj, new_file_name))
+                if not pynwb_check(os.path.join(new_nwb_dir, subj, new_file_name)): #if we cant read the NWB file with pynwb, we need to modify it to make it readable.
+                    # the nwbs are currently broken due to matnwb issues. 
+                    # We need to pack in an intracellular electrode to make them readable by pynwb, but this is a bit of a hack and we should eventually fix the matnwb export to include the intracellular electrode in the correct format. For now, we will just add a dummy intracellular electrode to each nwb file to make them readable by pynwb, and then we can remove the dummy electrode later if we want to.
+                    with h5py.File(os.path.join(new_nwb_dir, subj, new_file_name), 'r+') as nwb_file:
+                            # the electrode is there, but not linked to the acquisition data, which is what is causing the issue with pynwb. 
+                            #get the electrode object
+                            #try to find the 
+                            IntracellularEl, IEname = find_electrode_group(nwb_file)
+                            if IntracellularEl is None:
+                                IntracellularEl = IntracellularElectrode(name='unknown_electrode', 
+                                                                    description='Dummy electrode added to make the NWB file readable by pynwb. This electrode does not contain any real data and should be removed before analysis.',
+                                                                    device=Device(name='unknown_device'), location='unknown')
+                                #link the electrode to the electrode group
+                                #nwb_file['/general/intracellular_ephys'].copy(IntracellularEl, 'unknown_electrode')
+                                print("Added dummy intracellular electrode to NWB file to make it readable by pynwb.")
+                            else:
+                                #move the existing electrode to the correct location in the NWB file, which is under /general/intracellular_ephys/unknown_electrode, and link it to the acquisition data
+                                #IntracellularEl.parent.move(IEname, '/general/intracellular_ephys/nsfa')
+                                print("Moved existing electrode to correct location in NWB file and linked it to acquisition data.")
+                            electrode_group, _ = find_electrode_group(nwb_file)
+                            
+                            for key in range(nwb_file['/general/intracellular_ephys/intracellular_recordings/electrodes/electrode'].len()):
+                                nwb_file['/general/intracellular_ephys/intracellular_recordings/electrodes/electrode'][key] = electrode_group.ref
+                            for key in nwb_file['acquisition']:
+                                #link the electrode to the acquisition data
+                                #pop the electrode to 'delete' it, then re-add it as an IntracellularElectrode to link it properly
+                                try:
+                                    del nwb_file['acquisition'][key]['electrode']
+                                except:
+                                    print(f"No existing electrode reference found in acquisition data for {key}. Adding new reference to electrode group.")
+                                nwb_file['acquisition'][key]['electrode'] = h5py.SoftLink(electrode_group.name)
+                            for key in nwb_file['/stimulus/presentation/']:
+                                #link the electrode to the stimulus presentation data
+                                try:
+                                    del nwb_file['/stimulus/presentation/'][key]['electrode']
+                                except:
+                                    print(f"No existing electrode reference found in stimulus presentation data for {key}. Adding new reference to electrode group.")
+                                nwb_file['/stimulus/presentation/'][key]['electrode'] = h5py.SoftLink(electrode_group.name)
+                            #I think we also need to upda the refs here: /general/intracellular_ephys/intracellular_recordings/electrodes/electrode
+                            print("Linked electrode to acquisition and stimulus presentation data in NWB file.")
+                            print(electrode_group.ref)
+
+
+                            #save the modified NWB file
+                            nwb_file.flush()
+                    #attempt to read the modified NWB file with pynwb to ensure it is now readable
+                    with pynwb.NWBHDF5IO(os.path.join(new_nwb_dir, subj, new_file_name), 'r') as io:
+                        nwb_data = io.read() #if this works, then the NWB file is now readable by pynwb and we can move on to the next file. If it doesn't work, then we need to investigate further.
+                        print(f"Successfully read {os.path.join(new_nwb_dir, subj, new_file_name)} with pynwb.")
+
+            
+                #Even if it does open we need to modify to add subject weight
+                with h5py.File(os.path.join(new_nwb_dir, subj, new_file_name), 'r+') as nwb_file:
+                    if '/general/subject' in nwb_file:
+                        if 'weight' not in nwb_file['/general/subject']:
+                            nwb_file['/general/subject'].create_dataset('weight', data="0.0 kg")
+                            print(f"Added weight dataset to subject in NWB file {new_file_name}.")
+                        else:
+                            #get the current val and add units if not already there
+                            current_weight = nwb_file['/general/subject']['weight'][()]
+                            if isinstance(current_weight, bytes):
+                                current_weight = current_weight.decode('utf-8')
+                            if current_weight is None or current_weight == "" or current_weight == b'' or current_weight == b'NaN' or current_weight == 'NaN':
+                                nwb_file['/general/subject']['weight'][()] = "0.0 kg"
+                                print(f"Set weight dataset in subject in NWB file {new_file_name} to default value of 0.0 kg.")
+                            elif not isinstance(current_weight, str) or not current_weight.endswith("kg"):
+                                nwb_file['/general/subject']['weight'][()] = str(current_weight) + " kg"
+                                print(f"Updated weight dataset in subject in NWB file {new_file_name} to include units.")
+                    
+                    nwb_file.flush()
                 print(f"Copied {nwb} to {new_file_name}")
     return
 
